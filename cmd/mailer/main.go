28e926cea364c2f21d1d9c47ae41da61bb9e2d85
crazyoptimist <crazyoptimist@mail.com>
2021-10-21 05:23:25 -0500

    add sendgrid sdk

--- cmd/mailer/main.go
+++ cmd/mailer/main.go
@@ -6,10 +6,17 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/joho/godotenv"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 	"github.com/xuri/excelize/v2"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	path, err := os.Getwd()
 	if err != nil {
 		log.Println(err)
@@ -25,10 +32,23 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
 	for _, row := range rows {
-		for _, colCell := range row {
-			fmt.Print(colCell, "\t")
+		from := mail.NewEmail("crazyoptimist", "hey@crazyoptimist.net")
+		subject := "Sending with SendGrid is Fun"
+		to := mail.NewEmail("Dear", row[0])
+		plainTextContent := "and easy to do anywhere, even with Go"
+		htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+		message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+		response, err := client.Send(message)
+		if err != nil {
+			log.Fatal(err)
 		}
-		fmt.Println()
+		if response.StatusCode == 202 {
+			log.Println("Successfully delivered to: ", row[0])
+		} else {
+			log.Println("Email delivery failed: ", response.Body)
+		}
 	}
+
 }
